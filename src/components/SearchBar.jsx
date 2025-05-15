f4b77d4a9a3e010c05a4d689ea9167020270dd4f
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-05-15 09:47:55 +0300

    made the searchbar and styled

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+const SearchBar = ({ query, setQuery, searchMusic }) => {
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={(e) => e.key === "Enter" && searchMusic()}
+        placeholder="Search for a song or artist..."
+      />
+      <button onClick={searchMusic}> Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
