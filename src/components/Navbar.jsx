387a9dd0d4a0fdef75a24e77bf33d872f922a07e
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-05-15 16:41:39 +0300

    added links to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import { auth } from "../../firebaseConfig";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import DarkModeToggle from "./DarkModeToggle";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <nav className="navbar">
       <h1>Music Explorer</h1>
+      <div className="nav-links">
+        <Link to="/">Home</Link> {/* Added Home Link */}
+        {!user ? (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/favorites">Favorites</Link>
+            <Link to="/playlist">Playlist</Link>
+            <button onClick={() => signOut(auth)}>Logout</button>
+          </>
+        )}
+      </div>
       <DarkModeToggle />
     </nav>
   );
